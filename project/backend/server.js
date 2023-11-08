f6965764958f83b0b8e5fc1a9e7c8e5efbd55d54
Abdel-Rahman Mahmoud <abdelrahmandev6@gmail.com>
2023-11-08 17:06:53 +0200

    edit server 9

--- project/backend/server.js
+++ project/backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "dist"), { index: "index.html" }));
+
 // moragan configuration
 morgan.token("req-body", (req, res) => {
    return JSON.stringify(req.body);
